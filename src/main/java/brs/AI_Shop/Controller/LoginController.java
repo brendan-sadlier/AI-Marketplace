2bc04cc4df98ee105a9dac303280f72ca57a7dff
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2023-03-11 11:37:26 +0000

    Access Products in DB

---
+++ src/main/java/brs/AI_Shop/Controller/LoginController.java
@@ -0,0 +1,18 @@
+package brs.AI_Shop.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register.html";
+    }
+}
--- src/main/java/brs/AI_Shop/Controller/ShopController.java
+++ src/main/java/brs/AI_Shop/Controller/ShopController.java
@@ -1,4 +1,4 @@
-package brs.AI_Shop;
+package brs.AI_Shop.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +9,5 @@ public class ShopController {
     @GetMapping("/")
     public String index() {
         return "index.html";
-    }
-
-    @GetMapping("/products")
-    public String products() {
-        return "products.html";
-    }
-
-    @GetMapping("/item")
-    public String item() {
-        return "item.html";
     }
 }
