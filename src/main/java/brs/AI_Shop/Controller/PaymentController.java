172f28c49b2e98cfc0805627d26d616df1e7eac1
Alex O'Donnell <alex.odonnell1@ucdconnect.ie>
2023-03-15 19:44:21 +0000

    Working on payment.html with pop ups for payment portal

--- src/main/java/brs/AI_Shop/Controller/PaymentController.java
+++ src/main/java/brs/AI_Shop/Controller/PaymentController.java
@@ -1,15 +1,26 @@
 package brs.AI_Shop.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 
 public class PaymentController {
-
-
     @GetMapping("/payment")
     public String payment() {
         return "payment.html";
+    }
+
+    @PostMapping("/payment")
+    public void paymentComplete(HttpServletResponse response){
+        try{
+            response.sendRedirect("/payment");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
